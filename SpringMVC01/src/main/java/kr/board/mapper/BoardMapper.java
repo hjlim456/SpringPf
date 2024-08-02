16e6528c92efe1d7645764f1bdc6e3ad6d4a9152
hyojun <hjlim45678@gmail.com>
2024-08-02 09:51:15 +0900

    SpringMVC1 mysql db연결 완료

---
+++ SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package kr.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.board.entity.Board;
+
+@Mapper
+public interface BoardMapper {
+ 
+	List<Board> getLists();
+}
